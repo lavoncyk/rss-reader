390a5dc51798cbeea221c0597f42bf04a6a3df36
mrr2d2 <alaksandr.lavoncyk@gmail.com>
2021-09-13 17:54:54 +0300

    Rewrite get_db to create sqlalchemy session

--- rss-reader/rss_reader/database.py
+++ rss-reader/rss_reader/database.py
@@ -2,14 +2,25 @@
 Module with the DB configuration.
 """
 
+from typing import Generator
+
 import sqlalchemy as sa
 
 from rss_reader.config import settings
 
 
-def get_db() -> motor_async.AsyncIOMotorDatabase:
+def get_db() -> Generator[sa.orm.Session, None, None]:
     """
     Get DB instance.
     """
-    client = motor_async.AsyncIOMotorClient(settings.RSS_DB_URI)
-    return client["rss_reader_db"]
+    engine = sa.create_engine(settings.RSS_DB_URI, pool_pre_ping=True)
+    session_factory = sa.orm.sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine,
+    )
+    try:
+        db = session_factory()
+        yield db
+    finally:
+        db.close()
