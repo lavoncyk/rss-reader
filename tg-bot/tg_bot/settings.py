02fa91750417f62fbbd21006b324bcc3eb53ae02
mrr2d2 <alaksandr.lavoncyk@gmail.com>
2021-10-21 11:07:37 +0300

    Implement get_env_var() function for settings

--- tg-bot/tg_bot/settings.py
+++ tg-bot/tg_bot/settings.py
@@ -1,2 +1,26 @@
-TG_BOT_TOKEN = None
-FETCH_UPDATES_INTERVAL = 5
+"""
+Module with Telegram Bot settings.
+"""
+
+import os
+from typing import Any, Callable
+
+
+def get_env_var(
+    name: str,
+    *,
+    required: bool = False,
+    default: Any = None,
+    cast: Callable[[str], Any] = str,
+) -> Any:
+    """Get environment variable by name."""
+    value = os.environ.get(name, default)
+    if required and value is None:
+        raise ValueError(f"The '{name}' environment variable is required!")
+    return cast(value)
+
+
+TG_BOT_TOKEN = get_env_var("TG_BOT_TOKEN", required=True)
+FETCH_UPDATES_INTERVAL = get_env_var(
+    "FETCH_UPDATES_INTERVAL", cast=int, default=5
+)
