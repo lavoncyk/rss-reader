b0e199c5d88e740a4c318ab7cd9c7af1c9618b84
mrr2d2 <alaksandr.lavoncyk@gmail.com>
2021-10-25 15:43:35 +0300

    Add naive implementation for fetch feeds job

--- tg-bot/tg_bot/jobs.py
+++ tg-bot/tg_bot/jobs.py
@@ -1,7 +1,22 @@
+"""
+Module with bot background jobs.
+"""
 
-import telegram
 import telegram.ext
+
+from tg_bot.integrations import clients
 
 
 def fetch_feeds(context: telegram.ext.CallbackContext) -> None:
-    pass
+    """
+    Fetch feeds updates.
+
+    Args:
+        context (telegram.ext.CallbackContext): A tg bot context object.
+    """
+    posts_client = clients.PostsClient()
+    posts = posts_client.fetch_posts()
+
+    job = context.job
+    for post in posts:
+        context.bot.send_message(job.context, post["title"])
