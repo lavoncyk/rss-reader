cb51ddddc83e923877486c7944c3a6dba2c9c56a
mrr2d2 <alaksandr.lavoncyk@gmail.com>
2021-10-30 12:19:22 +0300

    Add CORS middleware

--- rss-reader/rss_reader/config.py
+++ rss-reader/rss_reader/config.py
@@ -2,14 +2,33 @@
 Module with RSS reader settings.
 """
 
+from typing import List, Union
+
 import pydantic
-from pydantic.networks import AnyUrl
+from pydantic.networks import AnyUrl, AnyHttpUrl
 
 
 class Settings(pydantic.BaseSettings):
     """
     Class which contains app settings.
     """
+
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", \
+    # "http://localhost:8080", "http://localhost:3000"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @pydantic.validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(
+        cls, value: Union[str, List[str]]
+    ) -> Union[List[str], str]:
+        """Parse the `BACKEND_CORS_ORIGINS` value."""
+        if isinstance(value, str) and not value.startswith("["):
+            return [i.strip() for i in value.split(",")]
+        elif isinstance(value, (list, str)):
+            return value
+        raise ValueError(value)
+
     RSS_DB_URI: AnyUrl
     RSS_TASKS_QUEUE_URI: AnyUrl
     RSS_TASKS_RES_BACKEND_URI: AnyUrl
--- rss-reader/rss_reader/main.py
+++ rss-reader/rss_reader/main.py
@@ -1,10 +1,25 @@
+"""
+Module with app configuring logic.
+"""
+
 import fastapi
+from fastapi.middleware import cors
 
 from rss_reader.api import post
 from rss_reader.api import rss_feed
+from rss_reader.config import settings
 
 
 app = fastapi.FastAPI()
 
 app.include_router(post.router)
 app.include_router(rss_feed.router)
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        cors.CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
