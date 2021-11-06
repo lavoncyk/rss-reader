82b0c601b2487facd2d8053afba782612ee9970d
mrr2d2 <alaksandr.lavoncyk@gmail.com>
2021-11-06 10:25:23 +0300

    Add menu with name on UI

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 import Board from "./components/Board";
+import './App.css'
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="App">
+        <div className="menu">
+          <div className="menu-logo">
+            <a href="/">NewsTerminal</a>
+          </div>
+          <div className="menu-center"></div>
+          <div className="menu-theme-switcher"></div>
+        </div>
         <Board />
       </div>
     );
